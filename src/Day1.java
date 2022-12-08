1b250df1060a668ddb67c646f82bed3a0ef1b8aa
HeyBanditoz <banditoz@protonmail.com>
2022-12-07 22:26:49 -0700

    day 1 part 2

--- src/Day1.java
+++ src/Day1.java
@@ -1,21 +1,25 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day1 {
     public static void main(String[] args) throws IOException {
-        int highest = 0;
+        List<Integer> elves = new ArrayList<>();
         int sum = 0;
         for (String line : Files.readAllLines(Path.of("day1.txt"))) {
             if (line.isBlank()) {
-                if (sum > highest) {
-                    highest = sum;
-                }
+                elves.add(sum);
                 sum = 0;
             } else {
                 sum += Integer.parseInt(line);
             }
         }
-        System.out.println(highest);
+        elves.stream()
+                .sorted()
+                .skip(elves.size() - 3)
+                .reduce(Integer::sum)
+                .ifPresentOrElse(System.out::println, () -> System.out.println(0));
     }
 }
